87a49aa1aafcc0e37eb22bd63216935f10bb6217
jasdeep singh <jasdeep.chauhan@hotmail.com>
2021-10-26 14:32:15 +0530

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace jasdeep.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResource =>
+            new List<ApiResource>
+            {
+                new ApiResource("myresourceapi")
+                {
+                    Scopes= new List<string>(){ "testingscope" }
+                }
+            };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope()
+                {
+                    Name="testingscope"
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client()
+                {
+                    ClientId="testing",
+                    ClientName="testing",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "testingscope" }
+                }
+            };
+
+    }
+}
